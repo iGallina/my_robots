95540467048bfbfcbeb22c18c3e9a47fbc1f760b
igallina <ian.gallina@gmail.com>
2012-12-07 11:41:43 -0200

    bot

--- el_mariachi.js
+++ el_mariachi.js
@@ -1,71 +1,56 @@
 var Robot = function(robot){
-  robot.turnLeft(robot.angle % 90);
-  
+    robot.turnLeft(robot.angle % 90);
+    robot.turnRight(360);
 };
-var ini_pos_ant = [];
 
 Robot.prototype.onIdle = function(ev) {
-  var robot = ev.robot;
-  if (robot.parentId) {
-    robot.ahead(1.618);
-    robot.turnGunRight(1.618);
-  }
-  else {
-    robot.ahead(-1.618);
-    robot.turnGunLeft(1.618);
-  }
+    var robot = ev.robot;
+    var count = 0;
+
+    if (robot.parentId) {
+        robot.ahead(40);
+        robot.turnGunRight(20);
+    } else {
+        robot.ahead(-40);
+        robot.turnGunLeft(20);
+    }
 };
 Robot.prototype.onWallCollision = function(ev) {
-  var robot = ev.robot;
-  if(!robot.parentId) {
-    robot.turnRight(ev.bearing - 90);
-  }
-  else {
-    robot.turnRight(ev.bearing + 90);
-  }
+    var robot = ev.robot;
+    if(!robot.parentId) {
+        robot.turnRight(ev.bearing - 90);
+    }
+    else {
+        robot.turnRight(ev.bearing + 90);
+    }
 };
 Robot.prototype.onRobotCollision = function(ev) {
-  var robot = ev.robot, collidedRobot = ev.collidedRobot;
-  robot.ignore('onRobotCollision')
-  if (ev.bearing > -90 && ev.bearing < 90) {
-    robot.back(100);
-  } 
-  else {
-    robot.ahead(100);
-  }
+    var robot = ev.robot, collidedRobot = ev.collidedRobot;
+    robot.ignore('onRobotCollision')
+    if (ev.bearing > -90 && ev.bearing < 90) {
+        robot.back(100);
+    } 
+    else {
+        robot.ahead(100);
+    }
 
-  if (robot.id != collidedRobot.parentId && robot.parentId != collidedRobot.id) {
-    robot.turnGunRight(ev.bearing - robot.cannonRelativeAngle);
-    robot.turnGunLeft(ev.bearing - robot.cannonRelativeAngle);
-  }
-  robot.listen('onRobotCollision')
+    if (robot.id != collidedRobot.parentId && robot.parentId != collidedRobot.id) {
+        robot.turnGunRight(ev.bearing - robot.cannonRelativeAngle);
+        robot.turnGunLeft(ev.bearing - robot.cannonRelativeAngle);
+    }
+    robot.listen('onRobotCollision')
 };
 Robot.prototype.onHitByBullet = function(ev) {
-  var robot;
-  robot.clone()
-  robot = ev.robot;
-  if(init_pos_ant[0]/2 == [scannedRobot.x+scannedRobot.y]/2){
-        robot.turn(45 + ev.bulletBearing);
-      robot.ahead(50);
-      robot.turn(45 + ev.bulletBearing);
-      robot.back(-90);      
-  }
-  robot.turn(45 - ev.bulletBearing);
-  robot.back(50);
-  robot.turn(45 - ev.bulletBearing);
-  robot.ahead(-90);
+    var robot = ev.robot;
+    robot.clone();
+    robot.turn(45 - ev.bulletBearing);
+    robot.back(90);
 };
-
 Robot.prototype.onScannedRobot = function(ev) {
-  var robot = ev.robot, scannedRobot = ev.scannedRobot;
-  robot.log();
-  if (robot.id == scannedRobot.parentId || robot.parentId == scannedRobot.id) {
-    return;
-  }
-  init_pos_ant = [scannedRobot.x+scannedRobot.y]
-  robot.fire();
-  if(init_pos_ant[0]/2 == [scannedRobot.x+scannedRobot.y]/2){
-    robot.turnGunRight(36);
-  }
-  robot.turnGunRight(29.0301727992);
+    var robot = ev.robot, scannedRobot = ev.scannedRobot;
+    if (robot.id == scannedRobot.parentId || robot.parentId == scannedRobot.id) {
+        return;
+    }
+    robot.fire();
+    robot.turnGunRight(20);
 };
---
+++ fight_code_bot.rb
@@ -0,0 +1,38 @@
+# encoding: utf-8
+
+require 'capybara'
+require 'capybara/dsl'
+require 'capybara/poltergeist'
+
+Capybara.run_server = false
+Capybara.current_driver = :poltergeist
+Capybara.javascript_driver = :poltergeist
+
+# delay pro JS carregar
+Capybara.default_wait_time = 5
+
+module Scraper
+    class SearchScraper
+        include Capybara::DSL
+        def search base_url, url
+            base_url = "fightcodegame.com"
+            url = "/profile/iGallina"
+            begin
+                Capybara.app_host = base_url
+                visit(url)
+                3.times do
+                    find_link("random fight").trigger('click')
+                end
+            rescue Capybara::Poltergeist::TimeoutError
+                puts "\t[ERRO] Timeout ao acessar o site #{base_url}#{url}."
+                search base_url, url
+            rescue Capybara::Poltergeist::JavascriptError
+                puts "\t[ERRO] Javascript quebrado no site #{base_url}#{url}."
+                search base_url, url
+            rescue Capybara::ElementNotFound
+                puts "\t[ERRO] Página não respondendo ou Elemento não encontrado no site #{base_url}#{url}."
+                search base_url, url
+            end
+        end
+    end
+end
