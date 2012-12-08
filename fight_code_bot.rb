96b1d945728cdacd703f8a8bcf93ce41615bc0b0
igallina <ian.gallina@gmail.com>
2012-12-08 11:38:35 -0200

    ops.. refactoring

--- fight_code_bot.rb
+++ fight_code_bot.rb
@@ -14,27 +14,30 @@ Capybara.default_wait_time = 5
 module Scraper
     class SearchScraper
         include Capybara::DSL
-        def search base_url, url
-            begin
-                Capybara.app_host = base_url
+        base_url = "http://fightcodegame.com"
+        url = "robots/randomfight/c0c72973e35962db255a"
+        begin
+            Capybara.app_host = base_url
+            while true do
                 visit(url)
-                3.times do
-                    find_link("random fight").trigger('click')
+                if page.url.contains?('timeout')
+                    sleep(15.seconds)
+                    return
                 end
-            rescue Capybara::Poltergeist::TimeoutError
-                puts "\t[ERRO] Timeout ao acessar o site #{base_url}#{url}."
-                search base_url, url
-            rescue Capybara::Poltergeist::JavascriptError
-                puts "\t[ERRO] Javascript quebrado no site #{base_url}#{url}."
-                search base_url, url
-            rescue Capybara::ElementNotFound
-                puts "\t[ERRO] Página não respondendo ou Elemento não encontrado no site #{base_url}#{url}."
-                search base_url, url
+                if page.body.contains?('Application')
+                   return
+                end
+                sleep(31.seconds)
             end
+        rescue Capybara::Poltergeist::TimeoutError
+            puts "\t[ERRO] Timeout ao acessar o site #{base_url}#{url}."
+            search base_url, url
+        rescue Capybara::Poltergeist::JavascriptError
+            puts "\t[ERRO] Javascript quebrado no site #{base_url}#{url}."
+            search base_url, url
+        rescue Capybara::ElementNotFound
+            puts "\t[ERRO] Página não respondendo ou Elemento não encontrado no site #{base_url}#{url}."
+            search base_url, url
         end
     end
-end
-
-while true do
-    search "fightcodegame.com", "/profile/iGallina"
 end
